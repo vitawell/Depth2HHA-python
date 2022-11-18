69c41a1b594adec1c4b56c2b97452cfedd3af522
vitawell <41141374+vitawell@users.noreply.github.com>
2022-11-18 14:04:29 +0800

    Create jet.py

---
+++ jet.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+#import glob
+import numpy as np
+#from PIL import Image
+ 
+def convertPNG(pngfile,outdir):
+    # READ THE DEPTH
+    im_depth = cv2.imread(pngfile)
+    
+    #apply colormap on depth image(image must be converted to 8-bit per pixel first)
+    im_color=cv2.applyColorMap(cv2.convertScaleAbs(im_depth,alpha=15),cv2.COLORMAP_JET)
+    
+    return im_color
+  
+    #convert to mat png
+    #im=Image.fromarray(im_color)
+    #save image
+    #im.save(os.path.join(outdir,os.path.basename(pngfile)))
+ 
+if __name__ == "__main__":
+    path = '/Users/Vita/Desktop/SfM/8.8 depth/'
+    path2 = '/Users/Vita/PycharmProjects/Depth2HHA-python-master/img/'
+
+    file_dir = os.listdir(path)
+    for file in file_dir:
+        if not os.path.isdir(file):  #需要是绝对路径
+            file_name = path + file
+            print(file_name)
+
+        (filenum, extension) = os.path.splitext(file)  #去掉文件后缀
+        # print(extension) #.tiff
+        
+        if extension == '.png':
+            im_color = convertPNG(file_name,path2)
+
+            cv2.imwrite(path2 + filenum + '.png', im_color)
