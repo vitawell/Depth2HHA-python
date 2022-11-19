31e7f61b6658915289b65d77df8b6e197263613d
vitawell <41141374+vitawell@users.noreply.github.com>
2022-11-19 10:49:48 +0800

    Update getHHA.py

--- getHHA.py
+++ getHHA.py
@@ -11,7 +11,11 @@ from utils.getCameraParam import *
 must use 'COLOR_BGR2GRAY' here, or you will get a different gray-value with what MATLAB gets.
 '''
 def getImage(file_name):
-    D = cv2.imread( file_name , cv2.COLOR_BGR2GRAY)/50000
+    
+    D = cv2.imread( file_name , cv2.COLOR_BGR2GRAY)
+    ##改为除以最大值？
+    M = np.max(D)
+    D = D/M
     # 海参灰度图最大值为47751，需要除以10000。最后单位需要是m，可能得除以100000。后面转rgb会乘100。
     # 改为除以50000，后面乘255，视觉效果更好。
     RD = D
@@ -80,7 +84,7 @@ if __name__ == "__main__":
 
             D, RD = getImage(file_name)
             camera_matrix = getCameraParam('color')
-            print('max gray value: ', np.max(D))        # make sure that the image is in 'meter'
+            #print('max gray value: ', np.max(D))        # make sure that the image is in 'meter'
             hha = getHHA(camera_matrix, D, RD)
             #hha_complete = getHHA(camera_matrix, D, D)
             cv2.imwrite(path2 + filenum + '.png', hha)
